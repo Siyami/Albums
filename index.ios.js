654588c24b4595a7a807ca9e5a62224561bd7d30
Siyami Avci <siyamiavci@gmail.com>
2017-05-11 16:16:08 -0700

    Add styling to Header Component

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,12 @@
 // Import a library to help create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/Header';
 
 // Create a Component
 const App = () => {
   return (
-    <Text>Some Text</Text>
+    <Header headerText={'Albums'}/>
   );
 };
 
